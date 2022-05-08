2617369938e4f9f526fce160ec7679c0f5c47311
alfredo <liocrazy321@gmail.com>
2022-05-07 21:48:43 -0500

    Spirng con propiedades

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -1,17 +1,36 @@
 package com.fundamentos.springboot.fundamentos;
 
+import com.fundamentos.springboot.fundamentos.bean.MyBean;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentos.springboot.fundamentos.component.ComponentTwoImplement;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Se utilizan las interfaces y esta tiene dependencias que son las clases que implementan la intefaz y se llaman los metodos de la dependencia seleccionada
+ *  se agrega el qualifier para indicar a que dependencia se hace referencia
+ *
+ */
+
 @SpringBootApplication
 public class FundamentosApplication implements CommandLineRunner {
 	private ComponentDependency componentDependency;
+	private MyBean myBean;
+	private MyBeanWithDependency myBeanWithDependency;
+	private MyBeanWithProperties myBeanWithProperties;
+
 	@Autowired
-	public FundamentosApplication(ComponentDependency componentDependency) {
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency, MyBean myBean,
+								  MyBeanWithDependency myBeanWithDependency,MyBeanWithProperties myBeanWithProperties) {
 		this.componentDependency = componentDependency;
+		this.myBean = myBean;
+		this.myBeanWithDependency = myBeanWithDependency;
+		this.myBeanWithProperties=myBeanWithProperties;
 	}
 
 	public static void main(String[] args) {
@@ -22,5 +41,8 @@ public class FundamentosApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		componentDependency.saludar();
+		myBean.print();
+		myBeanWithDependency.printWithDependency();
+		myBeanWithProperties.function();
 	}
 }
--- src/main/java/com/fundamentos/springboot/fundamentos/component/ComponentTwoImplement.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/component/ComponentTwoImplement.java
@@ -1,4 +1,15 @@
 package com.fundamentos.springboot.fundamentos.component;
 
-public class ComponentTwoImplement {
+import org.springframework.stereotype.Component;
+
+/**
+ * Se debe de indicar la notacion component para indicar que es parte de un componente general
+ */
+
+@Component
+public class ComponentTwoImplement implements ComponentDependency{
+    @Override
+    public void saludar() {
+        System.out.println("Hello Wordl desde mi componente dos");
+    }
 }
